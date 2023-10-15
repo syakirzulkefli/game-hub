86ca5d9de5bd559d9394e2a5c59169d067565db0
Mohamad Syakir <msyakirzulkefli@gmail.com>
2023-10-15 21:15:58 +0800

    Creating a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorScheme="whatsapp" color="black">
-      Button
-    </Button>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      color="black"
+      fontWeight={"bold"}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"gold"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
