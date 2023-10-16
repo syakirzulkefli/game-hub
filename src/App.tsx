0f2a20a5f211cb91774479a407c6b14e1963dbd2
Mohamad Syakir <msyakirzulkefli@gmail.com>
2023-10-16 08:14:44 +0800

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,16 @@ function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
-      color="black"
+
       fontWeight={"bold"}
     >
       <GridItem area={"nav"}>
-        <NavBar/>
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} bg={"gold"}>
-          Aside
-        </GridItem>
+        <GridItem area={"aside"}>Aside</GridItem>
       </Show>
-      <GridItem area={"main"} bg={"dodgerblue"}>
-        Main
-      </GridItem>
+      <GridItem area={"main"}>Main</GridItem>
     </Grid>
   );
 }
